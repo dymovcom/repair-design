81a20ade0510299e5c8fab8c827b20fa6fab7c0c
dymovcom <dymovcom@yandex.ru>
2020-05-04 11:54:22 +0500

    add GulpTask mincss

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
-const gulp = require("gulp");
+const gulp        = require("gulp");
 const browserSync = require('browser-sync').create();
+const cleanCSS    = require('gulp-clean-css');
+const rename      = require("gulp-rename");
 
 gulp.task("hello", function(done) {
   console.log("Привет, мир!");
@@ -7,11 +9,18 @@ gulp.task("hello", function(done) {
 });
 
 // Static server
-gulp.task('browser-sync', function() {
+gulp.task("browser-sync", function() {
   browserSync.init({
       server: {
           baseDir: "./"
       }
   });
   gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+gulp.task("mincss", function() {
+  return gulp.src("css/*.css")
+    .pipe(rename({suffix: ".min"}))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest("css/"))
 });
