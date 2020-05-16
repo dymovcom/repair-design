677dc93cc8cf24a1d9595d1f005217eb21b234b6
dymovcom <dymovcom@yandex.ru>
2020-05-16 22:14:49 +0500

    feat: add section steps

--- js/main.js
+++ js/main.js
@@ -58,7 +58,7 @@ $(document).ready(function () {
   }); 
 
   //initialize swiper when document ready
-  let mySwiper = new Swiper ('.swiper-container', {
+  let mySwiper = new Swiper ('.swiper-container__project', {
     // Optional parameters
     loop: true,
     pagination: {
@@ -71,15 +71,119 @@ $(document).ready(function () {
     },
   });
 
-  var next    = $('.swiper-button-next');
-  var prev    = $('.swiper-button-prev');
+  var next    = $('.swiper-button-next-project');
+  var prev    = $('.swiper-button-prev-project');
   var bullets = $('.swiper-pagination');
 
   next.css('left', prev.width() + 20 + bullets.width() + 20);
   bullets.css('left', prev.width() + 20);
 
-  // document.querySelector('.slide-1').addEventListener('click', () => {
-  //   mySwiper.slideTo(3);
-  //   // console.log(mySwiper);
-  // });
+
+  var mySwiperSteps = new Swiper ('.swiper-container__steps', {
+    // Optional parameters
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next-steps',
+      prevEl: '.swiper-button-prev-steps',
+    },
+  });
+
+  var mySwiperStepsImg = new Swiper ('.swiper-container__steps-img', {
+    // Optional parameters
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    },
+    pagination: {
+      el: '.swiper-pagination-step',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next-steps',
+      prevEl: '.swiper-button-prev-steps',
+    },
+  });
+
+  var nextStep    = $('.swiper-button-next-step');
+  var prevStep    = $('.swiper-button-prev-step');
+  var bulletsStep = $('.swiper-pagination');
+
+  nextStep.css('left', 280);
+  bulletsStep.css('left', prevStep.width() + 20);
+
+  
+
+
+  function activeSlide(index) {
+    let activeStep = document.querySelectorAll('.steps__step--active');
+    for (let step of activeStep) {
+      step.classList.toggle("steps__step--active");
+    }
+
+    $('.swiper-pagination-top').text(index + '/6');
+  }
+
+  document.querySelector('.swiper-button-prev-steps').addEventListener('click', () => {
+    index = mySwiperSteps.activeIndex;
+    if (index < 1) index = 6;
+    activeSlide(index);
+    indexStep = '.steps__step--' + index;
+    document.querySelector(indexStep).classList.toggle("steps__step--active");
+  });
+
+  document.querySelector('.swiper-button-next-steps').addEventListener('click', () => {
+    index = mySwiperSteps.activeIndex;
+    if (index > 6) index = 1;
+    activeSlide(index);
+    indexStep = '.steps__step--' + index;
+    document.querySelector(indexStep).classList.toggle("steps__step--active");
+  });
+
+
+  document.querySelector('.steps__step--1').addEventListener('click', () => {
+    activeSlide(1);
+    mySwiperSteps.slideTo(1);
+    mySwiperStepsImg.slideTo(1);
+    document.querySelector('.steps__step--1').classList.toggle("steps__step--active");
+  });
+  document.querySelector('.steps__step--2').addEventListener('click', () => {
+    activeSlide(2);
+    mySwiperSteps.slideTo(2);
+    mySwiperStepsImg.slideTo(2);
+    document.querySelector('.steps__step--2').classList.toggle("steps__step--active");
+  });
+  document.querySelector('.steps__step--3').addEventListener('click', () => {
+    activeSlide(3);
+    mySwiperSteps.slideTo(3);
+    mySwiperStepsImg.slideTo(3);
+    document.querySelector('.steps__step--3').classList.toggle("steps__step--active");
+  });
+  document.querySelector('.steps__step--4').addEventListener('click', () => {
+    activeSlide(4);
+    mySwiperSteps.slideTo(4);
+    mySwiperStepsImg.slideTo(4);
+    document.querySelector('.steps__step--4').classList.toggle("steps__step--active");
+  });
+  document.querySelector('.steps__step--5').addEventListener('click', () => {
+    activeSlide(5);
+    mySwiperSteps.slideTo(5);
+    mySwiperStepsImg.slideTo(5);
+    document.querySelector('.steps__step--5').classList.toggle("steps__step--active");
+  });
+  document.querySelector('.steps__step--6').addEventListener('click', () => {
+    activeSlide(6);
+    mySwiperSteps.slideTo(6);
+    mySwiperStepsImg.slideTo(6);
+    document.querySelector('.steps__step--6').classList.toggle("steps__step--active");
+  });
+  
 });
